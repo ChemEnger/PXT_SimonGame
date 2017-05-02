40355bf706f57e926470b7823668db4976507ab3
Simon <simonbrock@outlook.com>
2017-05-02 14:09:08 +0100

    Update main.ts
    Changed to incorporate new NameSpace of 'Simon' rather than 'Custom'.
    
    Should be more accurate timing for the regular button press

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-let timeOut = 0
+let m = 0
 let start = 0
 let i = 0
-let m = 0
+let timeOut = 0
+let corner = 0
 let NeoColor = 0
 let Neo: neopixel.Strip = null
 let freq = 0
 let length = 0
 let mistake = false
-let level = 0
 let gap = 0
 let init = 0
+let level = 0
 input.onButtonPressed(Button.A, () => {
     if (init == 1) {
         level = Math.max(1, level - 1)
@@ -23,7 +24,7 @@ input.onButtonPressed(Button.B, () => {
 input.onButtonPressed(Button.AB, () => {
     if (init == 1) {
         for (let k = 0; k <= 3; k++) {
-            custom.LightBeep(k)
+            null
         }
         basic.pause(500)
         init = 0
@@ -31,6 +32,8 @@ input.onButtonPressed(Button.AB, () => {
         control.reset()
     }
 })
+let pitch =[0]
+pitch =[311, 415, 247, 208, 42]
 init = 1
 gap = 0
 // A boolean to flag a mistake in repeating the
@@ -45,25 +48,25 @@ pins.analogSetPitchPin(AnalogPin.P0)
 Neo = neopixel.create(DigitalPin.P8, 4, NeoPixelMode.RGB)
 level = 1
 NeoColor = 0
-let sequence = [0]
+corner = 0
+let sequence =[0]
 while (init == 1) {
     basic.showNumber(level)
 }
 switch (level) {
     case 1:
-        length = 8
-        break
+    length = 8 - 1
+    break
     case 2:
-        length = 14
-        break
+    length = 14 - 1
+    break
     case 3:
-        length = 20
-        break
+    length = 20 - 1
+    break
     case 4:
-        length = 31
+    length = 31 - 1
 }
-setSequence()   //Create a full sequence, depending on the level
-m = 0
+setSequence()
 timeOut = 3000
 while (!(mistake)) {
     // Make the tones shorter for higher difficulty
@@ -74,23 +77,24 @@ while (!(mistake)) {
     } else {
         gap = 220
     }
-    //Sound / show the current sequence
+    // Sound / show the current sequence
     for (let j = 0; j <= i; j++) {
-        custom.LightBeep(sequence[j])
+        null
     }
+    // custom.Victory()
     start = input.runningTime()
     while (!(mistake) && i <= length) {
         if (pins.digitalReadPin(DigitalPin.P16) == 0) {
-            checkPress(0)
+            Simon.checkPress(0)
         }
         if (pins.digitalReadPin(DigitalPin.P1) == 0) {
-            checkPress(1)
+            Simon.checkPress(1)
         }
         if (pins.digitalReadPin(DigitalPin.P2) == 0) {
-            checkPress(2)
+            Simon.checkPress(2)
         }
         if (pins.digitalReadPin(DigitalPin.P12) == 0) {
-            checkPress(3)
+            Simon.checkPress(3)
         }
         if (input.runningTime() - start > timeOut) {
             mistake = true
@@ -103,38 +107,40 @@ while (!(mistake)) {
                 basic.showIcon(IconNames.Happy)
                 basic.pause(600)
                 basic.clearScreen()
+                if (i >= length) {
+                    for (let l = 0; l <= 2; l++) {
+                        for (let i02 = 0; i02 <= 3; i02++) {
+                            null
+                        }
+                    }
+                    for (let i1 = 0; i1 <= 1; i1++) {
+                        null
+                    }
+                    null
+                }
             }
             break;
         }
-    }
-    if (i >= length) {
-        break;
     }
 }
 basic.pause(500)
 if (mistake) {
     basic.showIcon(IconNames.Sad)
-    music.beginMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once)
+    // custom.Victory()
     basic.pause(100)
     basic.showNumber(i - 1)
 } else {
     basic.showIcon(IconNames.Happy)
-    music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    null
+    // music.beginMelody(music.builtInMelody(Melodies.Entertainer),
+    // MelodyOptions.Once)
     basic.pause(100)
     basic.showNumber(i)
 }
 function setSequence(): void {
     //Create a random sequence of (lights)
-    sequence = [] //Clear any previous sequence in memory
-    for (let l = 0; l < length; l++) {
+    sequence =[] //Clear any previous sequence in memory
+    for (let n = 0; n < length; n++) {
         sequence.push(Math.random(4))
     }
-}
-function checkPress(pressed: number): void {
-    //Check if the button pressed is correct
-    mistake = (pressed != sequence[m])
-    m += 1
-    custom.LightBeep(pressed)
-    //Reset the timeOut timer each time a button is pressed
-    start = input.runningTime()
 }
